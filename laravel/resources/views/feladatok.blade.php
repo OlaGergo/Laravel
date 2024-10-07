131dd1579edba684df1a75b0635ddc46ccee1e60
OlaGergo <olager397@hengersor.hu>
2024-10-07 11:24:50 +0200

    Delete laravel directory

--- laravel/resources/views/feladatok.blade.php
+++
@@ -1,6 +0,0 @@
-<?php
-$username = 'John';
-return view('tasklist')->withTasks($tasks);
-$foobar = 'foobar';
-return view('tasklist')->withTasks($tasks)->withFoo($foobar);
-?>
--- laravel/resources/views/welcome.blade.php
+++
@@ -1,4 +0,0 @@
-<body><div>Hi, <?php echo $name; ?></div>
-    <div>{{$foo}}</div></body>
-    Hi, <?=$name; ?>
-    Hi, {{$name}}
